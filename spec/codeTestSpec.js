9caf5238ee5929e5ff4b31a74b4faa2862fadf45
Max <maxnelson997@gmail.com>
2020-10-16 16:57:34 -0600

    added it description functions within describe statements and wrote initial functions for codeproblems

--- spec/codeTestSpec.js
+++ spec/codeTestSpec.js
@@ -1,14 +1,17 @@
 describe("CodeTest", function() {
     var codeTest = require('../src/codeTest');
     describe("MatchingBrackets", function() {
-        expect(codeTest.checkForMatchingBrackets('{}')).toBeTrue();
-        expect(codeTest.checkForMatchingBrackets('}{')).toBeFalse();
-        expect(codeTest.checkForMatchingBrackets('{{}')).toBeFalse();
-        expect(codeTest.checkForMatchingBrackets('')).toBeFalse();
-
+        it("should have matching brackets", function() {
+            expect(codeTest.checkForMatchingBrackets('{}')).toBeTrue();
+            expect(codeTest.checkForMatchingBrackets('}{')).toBeFalse();
+            expect(codeTest.checkForMatchingBrackets('{{}')).toBeFalse();
+            expect(codeTest.checkForMatchingBrackets('')).toBeFalse();
+        })
     })
     describe("SumOfMultiples", function() {
-        const BELOW_10 = 10
-        expect(codeTest.sumOfMultiples(3, 5, BELOW_10)).toEqual(23)
+        it("should have a sum of 23 for multiples of 3 and 5 below 10", function() {
+            const BELOW_10 = 10
+            expect(codeTest.sumOfMultiples(3, 5, BELOW_10)).toEqual(23)
+        })
     })
 })
---
+++ src/codeTest.js
@@ -0,0 +1,15 @@
+function checkForMatchingBrackets(strArg) {
+    let val = false;
+
+    return val;
+}
+
+function sumOfMultiples(first, second, below) {
+    return false;
+}
+
+
+module.exports = {
+    checkForMatchingBrackets,
+    sumOfMultiples
+}
