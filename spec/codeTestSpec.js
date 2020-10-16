4422708b8b75a80d2d9b92879c168035fc507f72
Max <maxnelson997@gmail.com>
2020-10-16 16:52:19 -0600

    tests written for matchingbrackets and sumofmultiples

--- spec/codeTestSpec.js
+++ spec/codeTestSpec.js
@@ -1,8 +1,14 @@
 describe("CodeTest", function() {
+    var codeTest = require('../src/codeTest');
     describe("MatchingBrackets", function() {
+        expect(codeTest.checkForMatchingBrackets('{}')).toBeTrue();
+        expect(codeTest.checkForMatchingBrackets('}{')).toBeFalse();
+        expect(codeTest.checkForMatchingBrackets('{{}')).toBeFalse();
+        expect(codeTest.checkForMatchingBrackets('')).toBeFalse();
 
     })
     describe("SumOfMultiples", function() {
-        
+        const BELOW_10 = 10
+        expect(codeTest.sumOfMultiples(3, 5, BELOW_10)).toEqual(23)
     })
 })
